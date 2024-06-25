10d0d83d8aa46679c7c97096a23b086bddc03753
Shadab Mizan <shadabsmizan@gmail.com>
2024-06-24 22:51:43 -0700

    Able to upload JSON data to the Flask server now, but HTML will not refresh automatically yet.

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,12 +1,19 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <TinyGPS++.h>
+#include <ArduinoJson.h>
+
 #include "personal_credentials.h"
+
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(1);
 
 void setup()
 {
     Serial.begin(115200);
 
+    // SSID and Password are stored in the personal credentials header file.
     Serial.print("Connecting to: "); Serial.println(WIFI_SSID);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
@@ -15,10 +22,72 @@ void setup()
         Serial.println("Connecting...");
         delay(1000);
     }
-    Serial.println("Connected.");
+    Serial.print("Connected to Wifi with local IP: "); Serial.println(WiFi.localIP());
+
+    // Set up GPS UART communication
+    SerialGPS.begin(9600, SERIAL_8N1, 16, 17); // GPIO16 is RX and GPIO17 is TX
+
+    
 }
 
+String gpsServer = "http://192.168.1.250:5000/gps";
+float latitude = 0;
+float longitude = 0;
+int satellites = 0;
 void loop()
 {
+    while (SerialGPS.available() > 0)
+    {
+        gps.encode(SerialGPS.read());
+    }
+    // See if we have a satellite found
+    satellites = gps.satellites.value();
+    Serial.println("Satellites found: " + String(satellites));
 
+    if (gps.location.isUpdated())
+    {
+        latitude = gps.location.lat();
+        longitude = gps.location.lng();
+
+        Serial.println("Latitude: " + String(latitude));
+        Serial.println("Longitude: " + String(longitude));
+
+        // Only make HTTP requests if we are still connected to the internet
+        if (WiFi.status() == WL_CONNECTED)
+        {
+            // Create a JSON object
+            DynamicJsonDocument jsonDoc(200);
+            jsonDoc["latitude"] = latitude;
+            jsonDoc["longitude"] = longitude;
+            jsonDoc["satellites"] = satellites;
+
+            // Serialization into a string
+            String payload;
+            serializeJson(jsonDoc, payload);
+
+            // Make an HTTP request
+            HTTPClient http;
+            Serial.println("Server Address: " + gpsServer);
+
+            http.begin(gpsServer);
+            http.addHeader("Content-Type", "application/json");
+            int httpResponseCode = http.POST(payload);
+
+            if (httpResponseCode > 0)
+            {
+                Serial.println("HTTP Response Code: " + String(httpResponseCode));
+                String response = http.getString();
+                Serial.println("Response: " + response);
+            } else 
+            {
+                Serial.println("HTTP Request Failed: " + String(http.errorToString(httpResponseCode)));
+            }
+
+            http.end();
+        }
+
+    }
+    
+    Serial.println();
+    delay(5000);
 }
--- WebServer/main.py
+++ WebServer/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, jsonify
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secretKey'
+
+# GPS Data storage in memory
+gpsData = {'latitude': None, 'longitude': None, 'satellites': None}
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -21,9 +25,26 @@ def home():
         # Implement a Post/Redirect/Get pattern, which helps prevent form resubmission and a clean url
         return redirect(url_for('home'))
 
+    return render_template('index.html', pos_data=session['pos_data'], satellites=gpsData['satellites'], lat=gpsData['latitude'], lng=gpsData['longitude'])
 
-    return render_template('index.html', pos_data=session['pos_data'])
+@app.route('/gps', methods=['GET', 'POST'])
+def gps():
+    try:
+        content = request.get_json()
+
+        gpsData['latitude'] = content['latitude']
+        gpsData['longitude'] = content['longitude']
+        gpsData['satellites'] = content['satellites']
+
+        print(f"Received: latitude = {gpsData['latitude']}, longitude = {gpsData['longitude']}, satellites = {gpsData['satellites']}\n")
+        return jsonify({'success' : True})
+    except Exception as e:
+        print(f"Error receiving data: {str(e)}")
+        return jsonify({'success' : False, 'error': str(e)})
+
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
